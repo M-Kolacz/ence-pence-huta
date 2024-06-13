f378f2c2796df972d9aa355cba20646766104f0c
Michał Kołacz <michal.kolacz45@gmail.com>
2024-06-13 14:42:02 +0200

    feat: :sparkles: add Storybook

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from 'vite'
 
 const MODE = process.env.NODE_ENV
 
+const isStorybook = process.argv[1]?.includes('storybook')
+
 export default defineConfig({
 	build: {
 		cssMinify: MODE === 'production',
@@ -28,27 +30,28 @@ export default defineConfig({
 		},
 	},
 	plugins: [
-		remix({
-			ignoredRouteFiles: ['**/*'],
-			serverModuleFormat: 'esm',
-			routes: async defineRoutes => {
-				return flatRoutes('routes', defineRoutes, {
-					ignoredRouteFiles: [
-						'.*',
-						'**/*.css',
-						'**/*.test.{js,jsx,ts,tsx}',
-						'**/__*.*',
-						// This is for server-side utilities you want to colocate
-						// next to your routes without making an additional
-						// directory. If you need a route that includes "server" or
-						// "client" in the filename, use the escape brackets like:
-						// my-route.[server].tsx
-						'**/*.server.*',
-						'**/*.client.*',
-					],
-				})
-			},
-		}),
+		!isStorybook &&
+			remix({
+				ignoredRouteFiles: ['**/*'],
+				serverModuleFormat: 'esm',
+				routes: async defineRoutes => {
+					return flatRoutes('routes', defineRoutes, {
+						ignoredRouteFiles: [
+							'.*',
+							'**/*.css',
+							'**/*.test.{js,jsx,ts,tsx}',
+							'**/__*.*',
+							// This is for server-side utilities you want to colocate
+							// next to your routes without making an additional
+							// directory. If you need a route that includes "server" or
+							// "client" in the filename, use the escape brackets like:
+							// my-route.[server].tsx
+							'**/*.server.*',
+							'**/*.client.*',
+						],
+					})
+				},
+			}),
 		process.env.SENTRY_AUTH_TOKEN
 			? sentryVitePlugin({
 					disable: MODE !== 'production',
