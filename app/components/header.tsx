a9a849e33ed79e5b792352e44ac552f57bfe3b3e
Michał Kołacz <michal.kolacz45@gmail.com>
2024-06-21 17:06:38 +0200

    feat: add Header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,16 @@
+import { Link } from '@remix-run/react'
 import { Button } from '#app/components/ui/button.tsx'
 import { Icon } from '#app/components/ui/icon.tsx'
 import Logo from './Logo.svg'
-import { NavigationLink } from './ui/navigation-link'
 import { IconButton } from './ui/icon-button'
+import { NavigationLink } from './ui/navigation-link'
 
 const navigationLinks = [
 	{ to: '/', children: 'Strona główna' },
 	{ to: '/aktualnosci', children: 'Aktualności' },
 	{ to: '/oferta', children: 'Oferta' },
-]
+	{ to: '/kontakt', children: 'Kontakt' },
+] as const
 
 export const Header = () => {
 	return (
@@ -20,18 +22,26 @@ export const Header = () => {
 			/>
 
 			<IconButton className="md:hidden">
-				<Icon name="menu" size="xl" />
+				<Icon name="menu" size="xl" title="Nawigacja strony" />
 			</IconButton>
 
 			<nav className="hidden list-none gap-6 space-x-8 md:flex">
-				{navigationLinks.map(link => (
-					<li key={link.to} className="flex items-center justify-center">
-						<NavigationLink {...link} className="text-xl font-bold" />
-					</li>
-				))}
-				<li>
-					<Button variant={'outline'}>Kontakt</Button>
-				</li>
+				{navigationLinks.map(link => {
+					if (link.to === '/kontakt')
+						return (
+							<li key={link.to}>
+								<Button variant={'outline'} asChild>
+									<Link {...link} />
+								</Button>
+							</li>
+						)
+
+					return (
+						<li key={link.to} className="flex items-center justify-center">
+							<NavigationLink {...link} className="text-xl font-bold" />
+						</li>
+					)
+				})}
 			</nav>
 		</header>
 	)
