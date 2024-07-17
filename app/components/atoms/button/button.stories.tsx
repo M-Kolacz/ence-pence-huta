0fda87b9a928dbd93646a8b6c260de0f752b58d6
Michał Kołacz <michal.kolacz45@gmail.com>
2024-07-17 17:12:03 +0200

    feat: add new Button

--- app/components/atoms/button/button.stories.tsx
+++ app/components/atoms/button/button.stories.tsx
@@ -1,6 +1,9 @@
 import { type Meta, type StoryObj } from '@storybook/react'
+import { Icon } from '#app/components/atoms'
 import { disableControls, setDesignPreview } from '#app/utils/storybook.ts'
+import { variations, sizes } from './button-variants.ts'
 import { Button } from './button.tsx'
+
 const meta = {
 	title: '⚛️ Atoms/Button',
 	component: Button,
@@ -10,14 +13,14 @@ const meta = {
 			'https://www.figma.com/file/zx5FkmiDiV7F8Jn5phnpeJ/Ence-Pence-Huta-v2?node-id=20-76&t=SgllQF4sLHtxY4V0-4',
 		),
 	},
-	args: { children: 'Click me', variant: 'primary', size: 'default' },
+	args: { children: 'Click me', variant: 'default', size: 'default' },
 	argTypes: {
 		variant: {
-			options: ['primary', 'secondary', 'outline', 'secondary-outline'],
+			options: variations,
 			control: { type: 'radio' },
 		},
 		size: {
-			options: ['default', 'sm', 'lg'],
+			options: sizes,
 			control: { type: 'radio' },
 		},
 		...disableControls<typeof Button>(['asChild']),
@@ -30,7 +33,18 @@ type Story = StoryObj<typeof meta>
 
 export const Component: Story = {
 	args: {
-		variant: 'primary',
+		variant: 'default',
+	},
+}
+
+export const IconButton: Story = {
+	args: {
+		variant: 'outline',
+		size: 'icon',
+		children: <Icon name="baby-shoes" />,
+	},
+	argTypes: {
+		...disableControls<typeof Button>(['size', 'children', 'variant']),
 	},
 }
 
@@ -38,14 +52,33 @@ export const Variants: Story = {
 	render: args => {
 		return (
 			<div className="flex gap-4">
-				<Button {...args} variant="primary" />
-				<Button {...args} variant="secondary" />
-				<Button {...args} variant="outline" />
-				<Button {...args} variant="secondary-outline" />
+				{variations.map(variant => (
+					<Button key={variant} {...args} variant={variant} />
+				))}
 			</div>
 		)
 	},
 	argTypes: {
 		...disableControls<typeof Button>(['variant']),
+	},
+}
+
+export const Sizes: Story = {
+	render: args => {
+		return (
+			<div className="flex gap-4">
+				{sizes.map(size => (
+					<Button
+						key={size}
+						{...args}
+						size={size}
+						children={size === 'icon' ? null : args.children}
+					/>
+				))}
+			</div>
+		)
+	},
+	argTypes: {
+		...disableControls<typeof Button>(['size']),
 	},
 }
