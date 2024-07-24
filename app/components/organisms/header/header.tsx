7161971295e9ad82f956c07e73d707743ec84651
Michał Kołacz <michal.kolacz45@gmail.com>
2024-07-24 21:14:20 +0200

    feat: add right side drawer

--- app/components/organisms/header/header.tsx
+++ app/components/organisms/header/header.tsx
@@ -1,9 +1,25 @@
 import { Link } from '@remix-run/react'
-import { Button, IconButton, NavigationLink, Icon } from '#app/components/atoms'
+import { useState } from 'react'
+import {
+	Button,
+	IconButton,
+	NavigationLink,
+	Icon,
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from '#app/components/atoms'
 import Logo from '../../Logo.svg'
 import { navigationLinks } from './header.helpers.ts'
 
 export const Header = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
 		<header className="flex items-center justify-between">
 			<img
@@ -12,9 +28,26 @@ export const Header = () => {
 				className="md:h-[121px] md:w-[150px]"
 			/>
 
-			<IconButton className="md:hidden" title="Nawigacja strony">
-				<Icon name="menu" />
-			</IconButton>
+			<Drawer open={isOpen} onOpenChange={setIsOpen} direction="right">
+				<DrawerTrigger asChild>
+					<IconButton className="md:hidden" title="Nawigacja strony">
+						<Icon name="menu" />
+					</IconButton>
+				</DrawerTrigger>
+				<DrawerContent>
+					<DrawerHeader className="text-left">
+						<DrawerTitle>Edit profile</DrawerTitle>
+						<DrawerDescription>
+							Make changes to your profile here. Click save when you're done.
+						</DrawerDescription>
+					</DrawerHeader>
+					<DrawerFooter className="pt-2">
+						<DrawerClose asChild>
+							<Button variant="outline">Cancel</Button>
+						</DrawerClose>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
 
 			<nav className="hidden md:block">
 				<ul className="list-none gap-6 md:flex">
