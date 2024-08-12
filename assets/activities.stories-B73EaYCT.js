f9f9959cdc1501dc0aad1ca484a7ba7aaec1105b
M-Kolacz <michal.kolacz45@gmail.com>
2024-08-12 18:33:44 +0000

    Deploying to gh-pages from @ M-Kolacz/ence-pence-huta@5d29ea1492114903385f4b7ed60c7236f61f2412 🚀

---
+++ assets/activities.stories-B73EaYCT.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-CGpYA4L6.js";import{s as j,a as o}from"./storybook-BaQXd8pY.js";import{v as g,A as w}from"./activity-card-_tvWgT5L.js";import{S as b,M as h}from"./main-pRcFfcdX.js";import{A as v}from"./app-DP2Ucc4X.js";import"./index-CleY8y_P.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./entry-preview-BXWVXEbm.js";import"./react-18-8-Ar02Sp.js";import"./index-SjZJwJVu.js";import"./modes-DycAkefs.js";import"./misc-BN-iWz0l.js";import"./index-Bb4qSo10.js";const f=()=>e.jsxs(b,{className:"flex flex-col gap-strong",children:[e.jsx("h2",{className:"max-w-prose text-center text-h2 font-bold text-text",children:"Wiemy, co jest najlepsze dla Twojego dziecka do nauki. Zobacz nasz multidyscyplinarny system edukacji"}),e.jsx("div",{className:"flex flex-wrap justify-evenly gap-strong",children:g.map(r=>e.jsx(w,{variant:r},r))})]});f.__docgenInfo={description:"",methods:[],displayName:"Activities"};const F={title:"🦠 Organisms/Activities",component:f,parameters:{layout:"fullscreen",...j("https://www.figma.com/file/zx5FkmiDiV7F8Jn5phnpeJ/Ence-Pence-Huta-v2?node-id=250-709&t=9r8tiuEMeZmOureV-4")},decorators:[r=>e.jsx(v,{children:e.jsx(h,{children:e.jsx(r,{})})})]},s={parameters:{...o("Mobile")}},t={parameters:{...o("Tablet")}},a={parameters:{...o("Desktop")}};var i,p,m;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Mobile')
+  }
+}`,...(m=(p=s.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var n,c,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Tablet')
+  }
+}`,...(l=(c=t.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,u,x;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Desktop')
+  }
+}`,...(x=(u=a.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};const J=["Mobile","Tablet","Desktop"];export{a as Desktop,s as Mobile,t as Tablet,J as __namedExportsOrder,F as default};
