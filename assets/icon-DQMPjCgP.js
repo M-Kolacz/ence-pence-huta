70dbc2b6216695f44e51075cc21d2ad85c069373
M-Kolacz <michal.kolacz45@gmail.com>
2024-09-11 18:09:05 +0000

    Deploying to gh-pages from @ M-Kolacz/ence-pence-huta@aabaa3dd3f497aae7b863a5f5b0e619234662f2e 🚀

--- assets/icon-DQMPjCgP.js
+++
@@ -1,4 +0,0 @@
-import{j as o}from"./jsx-runtime-CGpYA4L6.js";import{useMDXComponents as i}from"./index-DjKt9u9V.js";import{M as e,I as s,d as p}from"./index-D56GoR4q.js";import{I as c}from"./icon-Du6O1_GD.js";import{i as a}from"./name-BiYuBSRq.js";import"./index-CleY8y_P.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-30KEBH-I.js";import"../sb-preview/runtime.js";import"./index-SjZJwJVu.js";import"./index-CjAzGtZG.js";import"./index-BQrkOMu6.js";import"./index-D-8MO0q_.js";import"./index-BPhVFo-B.js";import"./index-DrFu-skq.js";import"./misc-opz8xCVi.js";function m(t){const n={h1:"h1",...i(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{title:"Tokens/Icons"}),`
-`,o.jsx(n.h1,{id:"iconography",children:"Iconography"}),`
-`,o.jsx("br",{}),`
-`,o.jsx(s,{children:a.map(r=>o.jsx(p,{name:r,children:o.jsx(c,{name:r})}))})]})}function k(t={}){const{wrapper:n}={...i(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(m,{...t})}):m(t)}export{k as default};
