05cb06a3bc15ace4f94036cc3f4e7652380467aa
Michał Kołacz <michal.kolacz45@gmail.com>
2024-09-11 20:05:02 +0200

    test: update tests

--- app/components/atoms/link/link.tsx
+++ app/components/atoms/link/link.tsx
@@ -6,7 +6,7 @@ export const Link = ({ className, ...props }: ComponentProps<'a'>) => {
 		<a
 			{...props}
 			className={cn(
-				'text-p1 text-primary-500 underline visited:text-[#A21CAF] hover:text-primary-600 focus:rounded-md focus:outline focus:outline-offset-2 focus:outline-ring active:text-error-500',
+				'text-p1 text-primary-600 underline transition-colors visited:text-[#A21CAF] hover:text-primary-700 focus:rounded-md focus:outline focus:outline-offset-2 focus:outline-ring active:text-error-600',
 				className,
 			)}
 		/>
