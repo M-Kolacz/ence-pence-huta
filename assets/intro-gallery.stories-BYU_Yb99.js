ca02cb6dfd3375cb4cb9d2ed376a57386d91dd1c
M-Kolacz <michal.kolacz45@gmail.com>
2024-11-30 09:46:40 +0000

    Deploying to gh-pages from @ M-Kolacz/ence-pence-huta@f980fe6b6e15a5d62d7ac0d74d7e2c741c8bcc29 🚀

---
+++ assets/intro-gallery.stories-BYU_Yb99.js
@@ -0,0 +1,13 @@
+import{j as s}from"./jsx-runtime-CGpYA4L6.js";import{s as f,a as o}from"./storybook-fP5FfkVL.js";import{I as g}from"./intro-gallery-BQQxA8bi.js";import{A as w}from"./app--kGS62sV.js";import"./index-CleY8y_P.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./entry-preview-CiarxA3d.js";import"./react-18-CzlTUK1-.js";import"./index-DMWgzpko.js";import"./modes-DycAkefs.js";import"./main-DnU6mDru.js";import"./misc-opz8xCVi.js";import"./button-BHZWfFOt.js";import"./index-CjAzGtZG.js";import"./index-Bb4qSo10.js";import"./icon-Du6O1_GD.js";const I={title:"Organisms/Gallery",component:g,parameters:{layout:"fullscreen",...f("https://www.figma.com/file/zx5FkmiDiV7F8Jn5phnpeJ/Ence-Pence-Huta-v2?node-id=327-4859&t=GhlP2VAdQe4cmQbA-4")},args:{},argTypes:{},decorators:[b=>s.jsx(w,{children:s.jsx(b,{})})]},e={parameters:{...o("Mobile")}},r={parameters:{...o("Tablet")}},t={parameters:{...o("Desktop")}};var a,p,m;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Mobile')
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var i,n,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Tablet')
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,d,u;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  parameters: {
+    ...setViewport('Desktop')
+  }
+}`,...(u=(d=t.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const J=["Mobile","Tablet","Desktop"];export{t as Desktop,e as Mobile,r as Tablet,J as __namedExportsOrder,I as default};
